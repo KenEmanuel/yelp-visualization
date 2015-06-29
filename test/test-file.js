7cef8ac5c57b5ec5c5dbc3f91c78b27234012c97
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-06-29 16:18:26 -0700

    Added test for initial route and response object from yelp search.

---
+++ test/test-file.js
@@ -0,0 +1,32 @@
+var superagent = require('superagent');
+var chai = require('chai'),
+    expect = chai.expect,
+    should = chai.should();
+
+
+describe('Get the index page', function() {
+    it('got a status 200', function(done) {
+        superagent.get('http://localhost:3000')
+            .send()
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.be.eql(200);
+                done();
+            });
+    });
+});
+
+describe('Get the restaurants page', function() {
+    it('can get back data', function(done) {
+        superagent.get('http://localhost:3000/restaurants/Irvine')
+            .send({
+                term: 'Indian',
+                location: 'Irvine'
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.body.length).to.be.eql(20);
+                done();
+            });
+    });
+});
