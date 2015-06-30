3faff91feb23dc3796abe5657337541b0bba671b
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-06-30 14:31:41 -0700

    #1 Basic search and data visualization.

--- public/app.js
+++ public/app.js
@@ -9,6 +9,28 @@ yelpApp.controller('curryController', function($http) {
             .success(function (data) {
                 console.log(data);
                 vm.data = data;
+                var ratings = [];
+
+                for (var i = 0; i < data.length; i++) {
+                    var rating = {};
+                    rating.name = data[i].name;
+                    rating.group = 'Indian';
+                    rating.value = data[i].numReview;
+                    rating.rating = 'Rating: ' +data[i].rating + ' stars';
+                    ratings.push(rating);
+                }
+                console.log(ratings);
+
+                var visualization = d3plus.viz()
+                    .container("div#viz")
+                    .data(ratings)
+                    .type("bubbles")
+                    .id(["group", "name", 'rating'])
+                    .depth(1)
+                    .size("value")
+                    .color("group")
+                    .height(600)
+                    .draw();
             })
             .error(function (data) {
                 console.log(data);
