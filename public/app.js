6ba479ab35637e292dbf7882463addb21925cc6b
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-07-01 13:01:26 -0700

    #1 Enabled search for location and two business types with visualization.

--- public/app.js
+++ public/app.js
@@ -1,39 +1,60 @@
 var yelpApp = angular.module('yelpApp', []);
 
-yelpApp.controller('curryController', function($http) {
+yelpApp.controller('yelpController', function($http) {
     var vm = this;
-    vm.message = 'Lets check out some curries';
+    vm.message = 'Lets check out some cuisines';
+    vm.foodTypes = [];
 
     vm.submit = function() {
-        $http.get('http://localhost:3000/restaurants/' + vm.location)
-            .success(function (data) {
-                console.log(data);
-                vm.data = data;
-                var ratings = [];
+        vm.foodTypes = [vm.foodOne, vm.foodTwo];
+        var businesses = [];
+        
+        for (var k = 0; k < vm.foodTypes.length; k++) {
+            $http.get('http://localhost:3000/restaurants/' + vm.location + '/' + vm.foodTypes[k])
+                .success(function(data) {
+                    vm.data = data;
 
-                for (var i = 0; i < data.length; i++) {
-                    var rating = {};
-                    rating.name = data[i].name;
-                    rating.group = 'Indian';
-                    rating.value = data[i].numReview;
-                    rating.rating = 'Rating: ' +data[i].rating + ' stars';
-                    ratings.push(rating);
-                }
-                console.log(ratings);
+                    for(var i = 0; i < data.length; i++) {
+                        var business = {};
+                        business.name = data[i].name;
+                        business.value = data[i].numReview;
+                        business.rating = 'Rating: ' +data[i].rating + ' stars';
+                        if(data[i].rating === 5) {
+                            business.color = '#ffd700';//gold
+                        } else if(data[i].rating === 4.5) {
+                            business.color = '#ff0000';//red
+                        } else if (data[i].rating === 4) {
+                            business.color = '#0000ff';//blue
+                        } else if (data[i].rating === 3.5) {
+                            business.color = '#7f00ff';//purple
+                        } else {
+                            business.color = '#ffffff';
+                        }
+                        businesses.push(business);
+                    }
+                    for(var j = 0; j < businesses.length; j++) {
+                        if(j < 20) {
+                            businesses[j].group = vm.foodOne;
+                        } else if (j >= 20 && j < 40) {
+                            businesses[j].group = vm.foodTwo;
+                        }
+                    }
+                    console.log(businesses);
 
-                var visualization = d3plus.viz()
-                    .container("div#viz")
-                    .data(ratings)
-                    .type("bubbles")
-                    .id(["group", "name", 'rating'])
-                    .depth(1)
-                    .size("value")
-                    .color("group")
-                    .height(600)
-                    .draw();
-            })
+                    var visualization = d3plus.viz()
+                        .container('div#viz')
+                        .data(businesses)
+                        .type('bubbles')
+                        .id(['group', 'name', 'rating'])
+                        .depth(1)
+                        .size('value')
+                        .color('color')
+                        .height(600)
+                        .draw();
+                })
             .error(function (data) {
                 console.log(data);
             });
     }
+}
 });
