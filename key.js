ac52a4422692aa83dd662b4d884db3f2c5f09577
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-07-14 15:07:36 -0700

    Added key.js for yelp oAUTH.

---
+++ key.js
@@ -0,0 +1,11 @@
+var exports = module.exports = {};
+
+exports.apiKey = {
+    consumer_key: "0w2RTA4_ReC994Iy9-XzhQ",
+    consumer_secret: "4biMNsVK5ISRaZtXklaa_W1wNhg",
+    token: "EVY5XPiS-CYgzdEx82qIp-tyaccAThY4",
+    token_secret: "oiv-C4YMBNlNa804Jk8H28ICuAk"
+};
+
+
+var biz = [];
